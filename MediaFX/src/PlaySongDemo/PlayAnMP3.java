47eb04aadb01183f7dc5257eea49510a8a48bd33
bradley ferraro <bradleyferraro@bradleys-MacBook-Pro.local>
2023-03-01 22:56:25 -0700

    Made small changes to PlayOneMP3.java

--- MediaFX/src/PlaySongDemo/PlayAnMP3.java
+++ MediaFX/src/PlaySongDemo/PlayAnMP3.java
@@ -27,12 +27,19 @@ public class PlayAnMP3 extends Application {
 
   @Override
   public void start(Stage stage) throws Exception {
+	  
     BorderPane pane = new BorderPane();
-    String path = "songfiles/Capture.mp3";
+    // I, Bradley Ferraro, changed the song to swingcheese
+    
+    String path = "songfiles/SwingCheese.mp3";
     pane.setCenter( new Label(path));
     playASong(path);
+    
+    // I also made the spacing how I like it.
+    
     // Put the pane in a sized Scene and show the GUI
     Scene scene = new Scene(pane, 255, 85); // 255 pixels wide, 85 pixels tall
+    
     stage.setScene(scene);
     // Don't forget to show the running app:
     stage.show();
---
+++ MediaFX/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author bradleyferraro
+ *
+ */
+module mediaFX {
+}
